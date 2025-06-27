cd18d5e011ae0be2f771c3061eaad2caf355d1e1
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-06-26 22:11:24 -0600

    Creación de clase Adopter
    Creacion de clase Adopter con todos los atributos correspondientes y sus debidos metodos Get y Set segun los requeria

---
+++ src/Persons/Adopter.java
@@ -0,0 +1,35 @@
+/**
+ * @author Greivin
+ */
+package Persons;
+
+public class Adopter extends Person {
+    private String phone;
+    private String type_of_housing;
+
+    public String getPhone() {
+        return phone;
+    }
+    public String getType_of_housing() {
+        return type_of_housing;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setType_of_housing(String type_of_housing) {
+        this.type_of_housing = type_of_housing;
+    }
+
+    public Adopter(String phone, String type_of_housing, String id, String nombre, String email) {
+        super(id, nombre, email);
+        this.phone = phone;
+        this.type_of_housing = type_of_housing;
+    }
+
+    @Override
+    public String toString() {
+        return "Adopter: " + super.toString() + "phone: " + phone + ", type_of_housing: " + type_of_housing;
+    }
+    
+}
